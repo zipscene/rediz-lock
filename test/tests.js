d52d8a7fd831a20cb7075ed780c8eaecbd470e20
Erin Thompson <erin.thompson@zipscene.com>
2015-09-29 13:24:08 -0400

    all functions written and tested and working

--- test/tests.js
+++ test/tests.js
@@ -11,6 +11,13 @@ const REDIZ_CONFIG = {
 };
 
 describe('Class Locker', () => {
+	let redizClient, locker;
+	beforeEach( (done) => {
+		redizClient = new RedizClient(REDIZ_CONFIG);
+		locker = new Locker(redizClient);
+		done();
+	});
+
 	it('construct a locker with a redis client, and a scriptWaiter', () => {
 		let redizClient = new RedizClient(REDIZ_CONFIG);
 		let locker = new Locker(redizClient);
@@ -41,9 +48,8 @@ describe('Class Locker', () => {
 	});
 
 	describe('#readLock', () => {
+
 		it('should lock read for a single key and release it', () => {
-			let redizClient = new RedizClient(REDIZ_CONFIG);
-			let locker = new Locker(redizClient);
 			let readLock;
 			return locker.readLock('myKey').then( (rwlock) => {
 				readLock = rwlock;
@@ -55,14 +61,17 @@ describe('Class Locker', () => {
 			}).then( () => {
 				expect(readLock.isLocked).to.equal(false);
 			}).catch( (error) => {
-				console.error(error);
-				throw error;
+				if (readLock) {
+					readLock.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
 
 		it('should lock read for a multiple keys and release them all', () => {
-			let redizClient = new RedizClient(REDIZ_CONFIG);
-			let locker = new Locker(redizClient);
 			let readLock;
 			return locker.readLock([ 'key1', 'key2', 'key3' ],
 				{ lockTimeout: 100, maxWaitTime: 20 }).then( (rwlock) => {
@@ -79,49 +88,59 @@ describe('Class Locker', () => {
 				}).then( () => {
 					expect(readLock.isLocked).to.equal(false);
 				}).catch( (error) => {
-					console.error(error);
-					throw error;
+					if (readLock) {
+						readLock.release().then( () => {
+							throw error;
+						});
+					} else {
+						throw error;
+					}
 				});
 		});
 
 		it('should time out when trying to access an already read locked key', () => {
-			let redizClient = new RedizClient(REDIZ_CONFIG);
-			let locker = new Locker(redizClient);
-			return locker.readLock([ 'key1', 'key1' ], { maxWaitTime: 0 }).then( (rwlock) => {
+			let readLock;
+			return locker.readLock([ 'key1', 'key1' ], { maxWaitTime: 0, lockTimeout: 12000000 }).then( (rwlock) => {
+				readLock = rwlock;
 				expect(rwlock).to.not.exist;
 			}).catch( (error) => {
-				expect(error).to.exist;
-				expect(error).to.be.an.instanceof(XError);
-				// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
-				expect(error.message).to.equal('Resource Locked');
+				if (readLock) {
+					readLock.release().then( () => {
+						throw error;
+					});
+				} else {
+					expect(error).to.exist;
+					expect(error).to.be.an.instanceof(XError);
+					// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
+					expect(error.message).to.equal('Resource Locked');
+				}
 			});
 		});
 
 		it('should time out when maxWaitTime is reached', function() {
 			this.timeout(5000);
-			let redizClient = new RedizClient(REDIZ_CONFIG);
-			let locker = new Locker(redizClient);
-			return locker.readLock([ 'key1', 'key1' ], { maxWaitTime: 2 }).then( (rwlock) => {
+			let readLock;
+			return locker.readLock([ 'key1', 'key1' ], { maxWaitTime: 2, lockTimeout: 12000000 }).then( (rwlock) => {
+				readLock = rwlock;
 				expect(rwlock).to.not.exist;
 			}).catch( (error) => {
-				expect(error).to.exist;
-				expect(error).to.be.an.instanceof(XError);
-				// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
-				expect(error.message).to.equal('Time out waiting for lock: key1');
+				if (readLock) {
+					readLock.release().then( () => {
+						throw error;
+					});
+				} else {
+					expect(error).to.exist;
+					expect(error).to.be.an.instanceof(XError);
+					// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
+					expect(error.message).to.equal('Time out waiting for lock: key1');
+				}
 			});
 		});
 	});
 
 	describe('#writeLock', () => {
-		let redizClient, locker;
-		beforeEach( (done) => {
-			redizClient = new RedizClient(REDIZ_CONFIG);
-			locker = new Locker(redizClient);
-			done();
-		});
 
-		it('should lock write for a single key', function() {
-			this.timeout(5000);
+		it('should lock write for a single key', () => {
 			let writeLocker;
 			return locker.writeLock('writeKey', { lockTimeout: 1 }).then( (rwlock) => {
 				writeLocker = rwlock;
@@ -130,16 +149,21 @@ describe('Class Locker', () => {
 				expect(rwlock.tokens.length).to.equal(1);
 				expect(rwlock.keys[0]).to.equal('writeKey');
 				expect(rwlock.locker).to.equal(locker);
-				console.log('here!!');
 				return rwlock.release();
 			}).then( () => {
-				console.log('here!!');
 				expect(writeLocker.isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (writeLocker) {
+					writeLocker.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
 
-		it('should create a lock for multiple keys', function() {
-			this.timeout(5000);
+		it('should create a lock for multiple keys', () => {
 			let writeLocker;
 			return locker.writeLock([ 'writeKey1', 'writeKey2' ], { lockTimeout: 1 }).then( (rwlock) => {
 				writeLocker = rwlock;
@@ -149,29 +173,139 @@ describe('Class Locker', () => {
 				return rwlock.release();
 			}).then( () => {
 				expect(writeLocker.isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (writeLocker) {
+					writeLocker.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
 
 		it('should fail when trying to lock a previously locked write locker', () => {
+			let writeLocker;
 			return locker.writeLock([ 'wKey1', 'wKey1' ], { maxWaitTime: 0 }).then( (rwlock) => {
+				writeLocker = rwlock;
 				expect(rwlock).to.not.exist;
 			}).catch( (error) => {
-				expect(error).to.exist;
-				expect(error).to.be.an.instanceof(XError);
-				// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
-				expect(error.message).to.equal('Resource Locked');
+				if (writeLocker) {
+					writeLocker.release().then( () => {
+						throw error;
+					});
+				} else {
+					expect(error).to.exist;
+					expect(error).to.be.an.instanceof(XError);
+					// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
+					expect(error.message).to.equal('Resource Locked');
+				}
 			});
 		});
 
 		it('should time out when maxWaitTime is reached', function() {
 			this.timeout(5000);
-			return locker.readLock([ 'wKey1', 'wKey1' ], { maxWaitTime: 2 }).then( (rwlock) => {
+			let writeLocker;
+			return locker.readLock([ 'wKey1', 'wKey1' ], { maxWaitTime: 2, lockTimeout: 12000000 }).then( (rwlock) => {
+				writeLocker = rwlock;
 				expect(rwlock).to.not.exist;
 			}).catch( (error) => {
+				if (writeLocker) {
+					writeLocker.release().then( () => {
+						throw error;
+					});
+				} else {
+					expect(error).to.exist;
+					expect(error).to.be.an.instanceof(XError);
+					// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
+					expect(error.message).to.equal('Time out waiting for lock: wKey1');
+				}
+			});
+		});
+	});
+
+	describe('#ReadLockWrap', () => {
+		it('should lock a read, run the function and release the key', () => {
+			return locker.readLockWrap('key', () => {
+				return 1;
+			}).then( (result) => {
+				expect(result).to.equal(1);
+			});
+		});
+
+		it('should lock a read, run a promise function that resolves and release the key', () => {
+			return locker.readLockWrap('key', (callback) => {
+				return new Promise( (resolve, reject) => {
+					setTimeout( () => {
+						return resolve(1);
+					}, 5);
+				});
+			}).then( (result) => {
+				expect(result).to.equal(1);
+			});
+		});
+
+		it('should lock a read, run a promise function that resolves and release the key', () => {
+			return locker.readLockWrap('key', (callback) => {
+				return new Promise( (resolve, reject) => {
+					setTimeout( () => {
+						return reject(new Error());
+					}, 5);
+				});
+			}).catch( (error) => {
 				expect(error).to.exist;
-				expect(error).to.be.an.instanceof(XError);
-				// expect(error.code).to.equal(XError.RESOURCE_LOCKED);
-				expect(error.message).to.equal('Time out waiting for lock: wKey1');
+			});
+		});
+
+		it('should lock a read, run the function that throws an errors and still release the key', () => {
+			return locker.readLockWrap('key', () => {
+				throw new Error('Oh! No!');
+			}).catch( (error) => {
+				expect(error).to.exist;
+				expect(error.message).to.equal('Oh! No!');
+			});
+		});
+	});
+
+	describe('#WriteLockWrap', () => {
+		it('should lock a write, run the function and release the key', () => {
+			return locker.writeLockWrap('key', () => {
+				return 1;
+			}).then( (result) => {
+				expect(result).to.equal(1);
+			});
+		});
+
+		it('should lock a write, run a promise function that rejects and release the key', () => {
+			return locker.readLockWrap('key', (callback) => {
+				return new Promise( (resolve, reject) => {
+					setTimeout( () => {
+						return resolve(1);
+					}, 5);
+				});
+			}).then( (result) => {
+				expect(result).to.equal(1);
+			});
+		});
+
+		it('should lock a write, run a promise function that rejects and release the key', () => {
+			return locker.readLockWrap('key', (callback) => {
+				return new Promise( (resolve, reject) => {
+					setTimeout( () => {
+						return reject(new Error());
+					}, 5);
+				});
+			}).catch( (error) => {
+				expect(error).to.exist;
+			});
+		});
+
+		it('should lock a write, run the function that errors and still release the key', () => {
+			return locker.writeLockWrap('key', () => {
+				throw new Error('Oh! No!');
+			}).catch( (error) => {
+				expect(error).to.exist;
+				expect(error.message).to.equal('Oh! No!');
 			});
 		});
 	});
@@ -186,9 +320,13 @@ describe('Class LockerSet', () => {
 		lockSet = locker.createLockSet();
 		done();
 	});
+
 	describe('Read Lockers', () => {
-		it('should add a locker to set after it\'s been created', () => {
+		it('should add a locker to set after it\'s been created', function() {
+			this.timeout(5000);
+			let readLock;
 			return locker.readLock('setKey').then( (rwlock) => {
+				readLock = rwlock;
 				lockSet.addLock(rwlock);
 				expect(lockSet.locks.length).to.equal(1);
 				expect(lockSet.locks[0].keys.length).to.equal(1);
@@ -198,13 +336,23 @@ describe('Class LockerSet', () => {
 				return lockSet.release();
 			}).then( () => {
 				expect(lockSet.locks[0].isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (lockSet.locks.length) {
+					lockSet.release().then( () => {
+						throw error;
+					});
+				} else if (readLock && readLock.isLocked) {
+					readLock.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
 
-		it('should create a locker and add it to the set automatically', () => {
-			let redizClient = new RedizClient(REDIZ_CONFIG);
-			let locker = new Locker(redizClient);
-			let lockSet = locker.createLockSet();
+		it('should create a locker and add it to the set automatically', function() {
+			this.timeout(5000);
 			return lockSet.readLock('setKey2').then( () => {
 				expect(lockSet.locks.length).to.equal(1);
 				expect(lockSet.locks[0].keys.length).to.equal(1);
@@ -214,24 +362,147 @@ describe('Class LockerSet', () => {
 				return lockSet.release();
 			}).then( () => {
 				expect(lockSet.locks[0].isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (lockSet.locks.length) {
+					lockSet.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
+			});
+		});
+
+		it('should create a readLock set and upgrade them all to write lock sets and release them', function() {
+			this.timeout(5000);
+			return lockSet.readLock([ 'key', 'key1', 'key2' ]).then( () => {
+				expect(lockSet.locks[0].keys.length).to.equal(3);
+				expect(lockSet.locks[0].isWriteLock).to.equal(false);
+				expect(lockSet.locks[0].isLocked).to.equal(true);
+				return lockSet.upgrade();
+			}).then( () => {
+				expect(lockSet.locks[0].isWriteLock).to.equal(true);
+				expect(lockSet.locks[0].keys[0]).to.equal('key');
+				expect(lockSet.locks[0].keys[1]).to.equal('key1');
+				expect(lockSet.locks[0].keys[2]).to.equal('key2');
+				expect(lockSet.locks[0].isLocked).to.equal(true);
+				return lockSet.release();
+			}).then( () => {
+				expect(lockSet.locks[0].isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (lockSet.locks.length) {
+					lockSet.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
 	});
 
 	describe('Write Lockers', () => {
-		it('should add a locker to set after it\'s been created', () => {
-			return locker.writeLock('setKey').then( (rwlock) => {
+		it('should add a locker to set after it\'s been created', function() {
+			this.timeout(5000);
+			let writeLock;
+			return locker.writeLock('write-key-set').then( (rwlock) => {
+				writeLock = rwlock;
 				lockSet.addLock(rwlock);
 				expect(lockSet.locks.length).to.equal(1);
 				expect(lockSet.locks[0].keys.length).to.equal(1);
-				expect(lockSet.locks[0].keys[0]).to.equal('setKey');
+				expect(lockSet.locks[0].keys[0]).to.equal('write-key-set');
 				expect(lockSet.locks[0].isWriteLock).to.equal(true);
 				expect(lockSet.locks[0].isLocked).to.equal(true);
 				return lockSet.release();
 			}).then( () => {
 				expect(lockSet.locks[0].isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (lockSet.locks.length) {
+					lockSet.release().then( () => {
+						throw error;
+					});
+				} else if (writeLock && writeLock.isLocked) {
+					writeLock.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
 			});
 		});
+
+		it('should create a write lock and add it to the set', function() {
+			this.timeout(5000);
+			return lockSet.lock('write-key-set1').then( () => {
+				expect(lockSet.locks.length).to.equal(1);
+				expect(lockSet.locks[0].keys.length).to.equal(1);
+				expect(lockSet.locks[0].keys[0]).to.equal('write-key-set1');
+				expect(lockSet.locks[0].isWriteLock).to.equal(true);
+				expect(lockSet.locks[0].isLocked).to.equal(true);
+				return lockSet.release();
+			}).then( () => {
+				expect(lockSet.locks[0].isLocked).to.equal(false);
+			}).catch( (error) => {
+				if (lockSet.locks.length) {
+					lockSet.release().then( () => {
+						throw error;
+					});
+				} else {
+					throw error;
+				}
+			});
+		});
+	});
+
+	describe('Read And Write', () => {
+		it('should add reads and writes to the set' +
+			' upgrade all the reads to write, and release all of them', function() {
+				this.timeout(10000);
+				return lockSet.lock([ 'write1', 'write2' ]).then( () => {
+					expect(lockSet.locks.length).to.equal(1);
+					expect(lockSet.locks[0].keys[0]).to.equal('write1');
+					expect(lockSet.locks[0].keys[1]).to.equal('write2');
+					return lockSet.writeLock([ 'write3', 'write4' ]);
+				}).then( () => {
+					expect(lockSet.locks.length).to.equal(2);
+					expect(lockSet.locks[1].keys.length).to.equal(2);
+					return lockSet.readLock([ 'read1', 'read2', 'read3' ]);
+				}).then( () => {
+					expect(lockSet.locks.length).to.equal(3);
+					expect(lockSet.locks[2].keys.length).to.equal(3);
+					expect(lockSet.locks[0].isWriteLock).to.equal(true);
+					expect(lockSet.locks[1].isWriteLock).to.equal(true);
+					expect(lockSet.locks[2].isWriteLock).to.equal(false);
+					return lockSet.upgrade();
+				}).then( () => {
+					expect(lockSet.locks.length).to.equal(3);
+					expect(lockSet.locks[2].keys[0]).to.equal('read1');
+					expect(lockSet.locks[2].keys[1]).to.equal('read2');
+					expect(lockSet.locks[2].keys[2]).to.equal('read3');
+					expect(lockSet.locks[2].isWriteLock).to.equal(true);
+					return lockSet.release();
+				}).then( () => {
+					expect(lockSet.locks.length).to.equal(3);
+					expect(lockSet.locks[0].keys[0]).to.equal('write1');
+					expect(lockSet.locks[0].keys[1]).to.equal('write2');
+					expect(lockSet.locks[1].keys[0]).to.equal('write3');
+					expect(lockSet.locks[1].keys[1]).to.equal('write4');
+					expect(lockSet.locks[2].keys[0]).to.equal('read1');
+					expect(lockSet.locks[2].keys[1]).to.equal('read2');
+					expect(lockSet.locks[2].keys[2]).to.equal('read3');
+					expect(lockSet.locks[0].isLocked).to.equal(false);
+					expect(lockSet.locks[1].isLocked).to.equal(false);
+					expect(lockSet.locks[2].isLocked).to.equal(false);
+				}).catch( (error) => {
+					if (lockSet.locks.length) {
+						lockSet.release().then( () => {
+							throw error;
+						});
+					} else {
+						throw error;
+					}
+				});
+			});
 	});
 });
 
