8a1267337cd4dca6764e851d3e378b9f7865a9ab
Christopher Breneman <crispy@cluenet.org>
2016-06-22 13:50:02 -0400

    Reset retry time when another process relocks

--- resources/lua/readLock.lua
+++ resources/lua/readLock.lua
@@ -1,10 +1,11 @@
 local numKeyArgs = 2
-if redis.call("exists", KEYS[1]) == 1 then
-	return 0
+local existingWriteLock = redis.call("get", KEYS[1])
+if existingWriteLock then
+	return { 0, existingWriteLock }
 else
 	redis.call("incr", KEYS[2])
 	if ARGV[1] ~= 0 and ARGV[1] ~= "0" then
 		redis.call("expire", KEYS[2], ARGV[1])
 	end
-	return 1
+	return { 1 }
 end
